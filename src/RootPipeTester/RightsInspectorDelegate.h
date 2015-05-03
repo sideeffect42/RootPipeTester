e2c7d9796b36c6cd3354de42eca0fc1d3a44b365
Takashi Yoshi <land.apfel@gmail.com>
2015-05-03 15:37:17 +0200

    Added: Rights Inspector

---
+++ src/RootPipeTester/RightsInspectorDelegate.h
@@ -0,0 +1,51 @@
+#import <Cocoa/Cocoa.h>
+#if MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_3
+#include <Security/AuthorizationDB.h>
+#endif
+
+@interface RPTKeyValuePair : NSObject {
+	id _key;
+	id _value;
+}
+- (id)initWithKey:(id)key value:(id)value;
+- (id)key;
+- (id)value;
+- (NSString *)description;
+- (void)dealloc;
+@end
+
+@interface RPTRightsDataSource : NSObject /*NSComboBoxDataSource*/ {
+	NSArray *_rights;
+}
+- (id)initWithRightsDB:(NSDictionary *)rightsDB;
+- (int)numberOfItemsInComboBox:(NSComboBox *)comboBox;
+- (id)comboBox:(NSComboBox *)comboBox objectValueForItemAtIndex:(int)index;
+- (void)dealloc;
+@end
+
+@interface RPTRightsViewDataSource : NSObject /*NSOutlineViewDataSource*/ {
+	NSDictionary *_right;
+	NSMutableSet *_pairs;
+}
+- (id)initWithRightDefinition:(NSDictionary *)rightDefinition;
+- (id)outlineView:(NSOutlineView *)outlineView child:(int)index ofItem:(id)item;
+- (BOOL)outlineView:(NSOutlineView *)outlineView isItemExpandable:(id)item;
+- (int)outlineView:(NSOutlineView *)outlineView numberOfChildrenOfItem:(id)item;
+- (id)outlineView:(NSOutlineView *)outlineView objectValueForTableColumn:(NSTableColumn *)tableColumn byItem:(id)item;
+
+- (void)dealloc;
+@end
+
+
+@interface RightsInspectorDelegate : NSObject {
+	IBOutlet NSWindow *inspectorWindow;
+    IBOutlet NSComboBox *rightChooser;
+    IBOutlet NSOutlineView *rightView;
+	
+	NSDictionary *_rightsDB;
+	NSString *_displayedRight;
+}
+- (IBAction)refreshView:(NSButton *)sender;
+- (NSDictionary *)rightDefinitionByName:(NSString *)name;
+- (void)dealloc;
+@end
---
+++ src/RootPipeTester/RightsInspectorDelegate.m
@@ -0,0 +1,209 @@
+#import "RightsInspectorDelegate.h"
+
+#define POLICY_DATABASE_FILE @"/private/etc/authorization"
+
+@implementation RPTKeyValuePair
+
+- (id)initWithKey:(id)key value:(id)value {
+	if ((self = [super init])) {
+		_key = [key retain];
+		_value = [value retain];
+	}
+	return self;
+}
+- (id)key {
+	return _key;
+}
+- (id)value {
+	return _value;
+}
+- (NSString *)description {
+	return [NSString stringWithFormat:@"{ %@ : %@ }", [self key], [self value]];
+}
+- (void)dealloc {
+	[_key release];
+	[_value release];
+	[super dealloc];
+}
+
+@end
+
+@implementation RPTRightsDataSource
+
+- (id)initWithRightsDB:(NSDictionary *)rightsDB {
+	if ((self = [super init])) {
+		_rights = [[rightsDB allKeys] retain];
+	}
+	return self;
+}
+- (id)init {
+	return [self initWithRightsDB:nil];
+}
+
+- (int)numberOfItemsInComboBox:(NSComboBox *)comboBox {
+	return [_rights count];
+}
+
+- (id)comboBox:(NSComboBox *)comboBox objectValueForItemAtIndex:(int)index {
+	return [_rights objectAtIndex:index];
+}
+
+- (unsigned)comboBox:(NSComboBox *)comboBox indexOfItemWithStringValue:(NSString *)string {
+	return [_rights indexOfObject:string];
+}
+
+- (void)dealloc {
+	[_rights release];
+	[super dealloc];
+}
+
+@end
+
+@implementation RPTRightsViewDataSource
+
+- (id)initWithRightDefinition:(NSDictionary *)rightDefinition {
+	if ((self = [super init])) {
+		_pairs = [[NSMutableSet alloc] initWithCapacity:[rightDefinition count] /* just a guess, most keys are scalar */];
+		_right = [[NSDictionary alloc] initWithDictionary:rightDefinition];
+	}
+	return self;
+}
+
+- (id)outlineView:(NSOutlineView *)outlineView child:(int)index ofItem:(id)item {
+	NSArray *orderedChildList = nil;
+	id key = nil, value = nil;
+	
+	item = (item ? [item value] : _right);
+	
+	if ([item isKindOfClass:[NSDictionary class]]) {
+		orderedChildList = [[item allKeys] sortedArrayUsingSelector:@selector(caseInsensitiveCompare:)];
+		key = [orderedChildList objectAtIndex:index];
+		value = [item objectForKey:key];
+	} else if ([item isKindOfClass:[NSArray class]]) {
+		orderedChildList = [item sortedArrayUsingSelector:@selector(caseInsensitiveCompare:)];
+		key = @"";
+		value = [orderedChildList objectAtIndex:index];
+	} else {
+		key = item;
+		value = @"";
+	}
+	
+	RPTKeyValuePair *pair = [[RPTKeyValuePair alloc] initWithKey:key value:value];
+	[_pairs addObject:pair]; // store pair in _pairs for memory management
+	[pair release];
+	
+	return pair;
+}
+
+- (BOOL)outlineView:(NSOutlineView *)outlineView isItemExpandable:(id)item {
+	return ([self outlineView:outlineView numberOfChildrenOfItem:item] > 0);
+}
+
+- (int)outlineView:(NSOutlineView *)outlineView numberOfChildrenOfItem:(id)item {
+	item = (item ? [item value] : _right);
+	return ([item respondsToSelector:@selector(count)] ? [item count] : 0);
+}
+
+- (id)outlineView:(NSOutlineView *)outlineView objectValueForTableColumn:(NSTableColumn *)tableColumn byItem:(id)item {
+	NSString *colId = [tableColumn identifier];
+	id object = nil;
+	
+	if ([colId isEqualToString:@"key"]) {
+		object = [item key];
+	} else if ([colId isEqualToString:@"value"]) {
+		object = [item value];
+	}
+	
+	if (!object) {
+		return @""; // object is nil
+	} else if ([object isKindOfClass:[NSString class]]) {
+		return object;
+	} else if (CFGetTypeID(object) == CFBooleanGetTypeID()) {
+		return ([object boolValue] ? @"true" : @"false");
+	} else if ([object isKindOfClass:[NSArray class]]) {
+		return @"(Array)";
+	} else if ([object isKindOfClass:[NSDictionary class]]) {
+		return @"(Dictionary)";
+	} else {
+		return [NSString stringWithFormat:@"%@", object]; // maybe the object can be formatted to a string
+	}
+}
+- (void)dealloc {
+	[_pairs release];
+	[_right release];
+	[super dealloc];
+}
+
+@end
+
+
+
+@implementation RightsInspectorDelegate
+
+- (id)init {
+	if ((self = [super init])) {
+		_rightsDB = nil;
+		_displayedRight = nil;
+	}
+	return self;
+}
+
+- (BOOL)loadPolicyDBFromPath:(NSString *)path {
+	[_rightsDB release];
+	_rightsDB = [(([(_rightsDB = [NSDictionary dictionaryWithContentsOfFile:path]) objectForKey:@"rights"]) ?: _rightsDB) retain]; // 10.2.x doesn't have a rights subkey
+	
+	return ([_rightsDB isKindOfClass:[NSDictionary class]] && [_rightsDB count] > 0);
+}
+
+- (void)awakeFromNib {
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateOutlineView:) name:NSComboBoxSelectionDidChangeNotification object:rightChooser];
+	
+	// Load Policy Database
+	if (![self loadPolicyDBFromPath:POLICY_DATABASE_FILE]) { NSLog(@"Error while loading Policy DB"); }
+	
+	// Initialise Combo Box
+	[rightChooser setCompletes:YES];
+	[rightChooser setUsesDataSource:YES];
+	[rightChooser setDataSource:[[RPTRightsDataSource alloc] initWithRightsDB:_rightsDB]];
+	[rightChooser reloadData];
+	
+	// Select "system.preferences" by default
+	[rightChooser selectItemAtIndex:[[rightChooser dataSource] comboBox:rightChooser indexOfItemWithStringValue:@"system.preferences"]];
+}
+
+- (NSDictionary *)rightDefinitionByName:(NSString *)name {
+	NSDictionary *rightDefinition = nil;
+	
+	AuthorizationRightGet([name UTF8String], (CFDictionaryRef *)&rightDefinition); // rightDefinition is autoreleased
+	
+	return rightDefinition;
+}
+
+- (void)updateOutlineViewWithRightName:(NSString *)rightName {
+	NSDictionary *rightDefinition = [self rightDefinitionByName:rightName];	
+	[rightView setDataSource:[[RPTRightsViewDataSource alloc] initWithRightDefinition:rightDefinition]];
+	
+	// Update _displayedRight
+	[_displayedRight release];
+	_displayedRight = [rightName retain];
+}
+
+- (void)updateOutlineView:(NSNotification *)notification {
+	NSComboBox *comboBox = [notification object];
+	NSString *rightName = [[comboBox dataSource] comboBox:comboBox objectValueForItemAtIndex:[comboBox indexOfSelectedItem]];
+	[self updateOutlineViewWithRightName:rightName];
+}
+
+- (IBAction)refreshView:(NSButton *)sender {
+    [self updateOutlineViewWithRightName:_displayedRight];
+}
+
+- (void)dealloc {
+	[[rightChooser dataSource] release];
+	[[rightView dataSource] release];
+	[_rightsDB release];
+	[_displayedRight release];
+	[super dealloc];
+}
+
+@end
